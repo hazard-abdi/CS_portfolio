d02e4c4ee1ed33393218a0feb7094d9da9fead91
hazard-abdi <ha899047@gmail.com>
2026-01-05 03:58:45 +0800

    Make build script executable

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // Replace 'portfolio' with your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Change this to match your repo name!
+  base: '/CS_portfolio/', // Change this to match your repo name!
 });
